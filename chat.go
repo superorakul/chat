82cc762bc523669af485d3e0f0d4506b8b29ee3b
superorakul <buslaev81@gmail.com>
2015-04-27 23:51:31 +0400

    Update chat.go

--- chat.go
+++ chat.go
@@ -1,1 +1,39 @@
-al kakashka
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"os"
+)
+
+func main() {
+
+	service := ":1200"
+	listener, err := net.Listen("tcp", service)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+
+	}
+
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	readMessage := make(chan string)
+	writeMessage := make(chan string)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
